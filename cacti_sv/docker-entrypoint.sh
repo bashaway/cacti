765ee51a8d3a5b78a2e265c4eb2ea3be87a4aa62
root <root@vmnetserv01.prosper2.net>
2020-05-06 14:59:10 +0900

    separate rra directory

--- cacti_sv/docker-entrypoint.sh
+++ cacti_sv/docker-entrypoint.sh
@@ -7,6 +7,7 @@ done
 if [ "`mysql -ucactiuser -pcactipwd  -h cacti_db cacti  -e 'show tables'`" = "" ]  ; then
   mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -prootpwd mysql -h cacti_db
   cat /usr/share/doc/cacti/cacti.sql |  mysql -ucactiuser -pcactipwd cacti -h cacti_db
+  chown -R apache.apache /var/lib/cacti/rra
 fi
 
 exec "$@"
